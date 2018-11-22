dfd2bda60e90fe6c5d30f022f0830443b732231c
MOnA <yiyuanweng1995@gmail.com>
2018-11-21 21:32:10 -0600

    finishing container.js, add part of server.js

--- container.js
+++ container.js
@@ -3,13 +3,19 @@ const path = require('path');
 
 const container = dependable.container();
 
-const simpleDependencies = [['_', 'lodash'], [''], []];
+const simpleDependencies = [['_', 'lodash']];
 
 simpleDependencies.map(val => {
   container.register(val[0], () => require(val[1]));
-});//for each var, register [var[0], var[1]]
+}); //for each var, register [var[0], var[1]]
 
 //const _ = require('lodash');
 
-container.load(path.join(__dirname,'/controllers'));
-container.load(path.join(__dirname,'/helpers'))
+container.load(path.join(__dirname, '/controllers'));
+container.load(path.join(__dirname, '/helpers'));
+
+container.register('conatiner', () => {
+  return container;
+});
+
+module.exports = container;
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+container.resolve(() => {
+  const app = SetupExpress();
+  function SetupExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(3000, () => {
+      console.log('Listening on port 3000');
+    });
+  }
+});
